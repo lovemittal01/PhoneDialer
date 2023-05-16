6096cbc9a0df7bdb841bbf4a1c1017e83f293e1e
Love Mittal <78198296+lovemittal01@users.noreply.github.com>
2023-05-16 23:40:04 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,71 @@
+const key1 = document.querySelector('.r1c1')
+
+key1.addEventListener("click", () => {
+    document.querySelector(".upper").innerText = "1"
+})
+
+const key2 = document.querySelector('.r1c2')
+
+key2.addEventListener("click", () => {
+    document.querySelector(".upper").innerText = +"2"
+})
+
+const key3 = document.querySelector('.r1c3')
+
+key3.addEventListener("click", () => {
+    document.querySelector(".upper").innerText = "3"
+})
+
+const key4 = document.querySelector('.r2c1')
+
+key4.addEventListener("click", () => {
+    document.querySelector(".upper").innerText = "4"
+})
+
+const key5 = document.querySelector('.r2c2')
+
+key5.addEventListener("click", () => {
+    document.querySelector(".upper").innerText = "5"
+})
+
+const key6 = document.querySelector('.r2c3')
+
+key6.addEventListener("click", () => {
+    document.querySelector(".upper").innerText = "6"
+})
+
+const key7 = document.querySelector('.r3c1')
+
+key7.addEventListener("click", () => {
+    document.querySelector(".upper").innerText = "7"
+})
+
+const key8 = document.querySelector('.r3c2')
+
+key8.addEventListener("click", () => {
+    document.querySelector(".upper").innerText = "8"
+})
+
+const key9 = document.querySelector('.r3c3')
+
+key9.addEventListener("click", () => {
+    document.querySelector(".upper").innerText = "9"
+})
+
+const key10 = document.querySelector('.r4c1')
+
+key10.addEventListener("click", () => {
+    document.querySelector(".upper").innerText = "*"
+})
+
+const key11 = document.querySelector('.r4c2')
+
+key11.addEventListener("click", () => {
+    document.querySelector(".upper").innerText = "0"
+})
+
+const key12 = document.querySelector('.r4c3')
+
+key12.addEventListener("click", () => {
+    document.querySelector(".upper").innerText = "#"
+})
